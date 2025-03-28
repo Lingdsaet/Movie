7e0a690a836a992312797cdc1a7e9a0ae949fc4e
Lingdsaet <160372942+Lingdsaet@users.noreply.github.com>
2025-03-28 14:12:11 +0700

    Add files via upload

---
+++ Repository/IMovieHome.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Movie.RequestDTO;
+
+
+namespace Movie.Repository
+{
+    public interface IMovieHome
+    {
+        //Home
+        Task<IEnumerable<string>> GetPostersAsync();
+        Task<IEnumerable<RequestMovieDTO>> GetNewMovieAsync();
+        Task<IEnumerable<RequestMovieDTO>> GetHotMovieAsync();
+        Task<IEnumerable<RequestMovieDTO>> GetSeriesAsync();
+        Task<IEnumerable<RequestMovieDTO>> GetActionMovieAsync();
+    }
+}
