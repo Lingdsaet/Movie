b8efa62a23d594578285f8164a130edcdb80099a
Lingdsaet <160372942+Lingdsaet@users.noreply.github.com>
2025-04-10 09:30:07 +0700

    Add files via upload

---
+++ IUserUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Movie.Models;
+using Movie.ResponseDTO;
+
+namespace Movie.Repository
+{
+    public interface IUserUserRepository
+    {
+        Task<RequestUserDTO?> LoginAsync(string email, string password);
+        Task<RequestUserDTO?> RegisterAsync(string username, string email, string password);
+    }
+}
